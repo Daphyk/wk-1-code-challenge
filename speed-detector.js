6059dd6dbe77f5200586c4e13d3069463ffc32b4
Daphyk <kendid07gmail.com>
2023-09-23 09:45:47 +0300

    Updated speed-detector file

---
+++ speed-detector.js
@@ -0,0 +1,30 @@
+function detectDemeritPoints(speed) {
+    const speedLimit = 70;
+    const kmPerDemeritPoint = 5;
+  
+    if (speed <= speedLimit) {
+      return "Ok";
+    } else {
+      const demeritPoints = Math.floor((speed - speedLimit) / kmPerDemeritPoint);
+      if (demeritPoints <= 12) {
+        return `Points: ${demeritPoints}`;
+      } else {
+        return "License suspended";
+      }
+    }
+  }
+  
+  function speedDetector() {
+    const input = prompt("Enter the car speed in (km/h):");
+    const speed = parseInt(input);
+  
+    if (!isNaN(speed)) {
+      const result = detectDemeritPoints(speed);
+      alert(result);
+    } else {
+      alert("Invalid input. Enter a valid speed as a number.");
+    }
+  }
+  
+  speedDetector();
+  
